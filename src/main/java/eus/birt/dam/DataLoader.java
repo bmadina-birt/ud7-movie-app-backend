f40449eb0510f75bb0ce278aab6961e31eeaf82a
bmadina <bmadina@birt.eus>
2025-03-18 16:43:49 +0100

    cambio en hibernate y DataLoader

--- src/main/java/eus/birt/dam/DataLoader.java
+++ src/main/java/eus/birt/dam/DataLoader.java
@@ -6,11 +6,13 @@ import eus.birt.dam.service.OmdbService;
 import eus.birt.dam.service.PeliculaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Profile("!prod")  // No se ejecutará en producción
 public class DataLoader implements CommandLineRunner {
 
     @Autowired
